e1650aff937af8a584191d68b6d7054e5910e2d0
Mark-André Hopf <mhopf@mark13.org>
2026-06-13 06:15:07 +0200

    clean up test

--- spec/gl/algorithms/coordinates.spec.ts
+++ spec/gl/algorithms/coordinates.spec.ts
@@ -1,8 +1,7 @@
 import { describe, expect, it } from "vitest"
-import { mat4, vec3, vec4 } from "gl-matrix"
+import { mat4, vec3 } from "gl-matrix"
 import { deg2rad } from "src/gl/algorithms/deg2rad"
 import type { Size } from "src/gl/types/Size"
-import { intersectLineAndPlane } from "src/gl/algorithms/intersectLineAndPlane"
 import { screen2pointInPlane, screen2world, world2screen } from "src/gl/algorithms/coordinates"
 
 describe("world/screen coordinate system conversions", () => {
@@ -24,16 +23,7 @@ describe("world/screen coordinate system conversions", () => {
     })
     it("3d to 2d to 3d", () => {
         const point = vec3.fromValues(3, 7, 10)
-        const screen = world2screen(point, perspective, canvas)
-        const world = screen2world(screen, perspective, canvas)
-
-        const out = intersectLineAndPlane(
-            vec3.create(), // out
-            vec3.create(), // line origin
-            world, // line direction
-            vec3.fromValues(0, 0, point[2]), // plane origin
-            vec3.fromValues(0, 0, 1) // plane normal
-        )
+        const out = fromWorldToScreenAndBack(point, perspective, mat4.create(), canvas)
         // console.log(out ? vec3.str(out) : out)
 
         expect(point[0]).to.be.closeTo(out![0], 0.01)
@@ -47,7 +37,7 @@ describe("world/screen coordinate system conversions", () => {
         const camera = mat4.create()
         mat4.translate(camera, camera, vec3.fromValues(1, 2, -5))
 
-        const out = doIt(point, perspective, camera, canvas)
+        const out = fromWorldToScreenAndBack(point, perspective, camera, canvas)
 
         expect(point[0]).to.be.closeTo(out![0], 0.1)
         expect(point[1]).to.be.closeTo(out![1], 0.1)
@@ -62,7 +52,7 @@ describe("world/screen coordinate system conversions", () => {
         mat4.rotateY(camera, camera, deg2rad(20))
         mat4.rotateZ(camera, camera, deg2rad(30))
 
-        const out = doIt(point, perspective, camera, canvas)
+        const out = fromWorldToScreenAndBack(point, perspective, camera, canvas)
         // console.log(out ? vec3.str(out) : out)
 
         expect(point[0]).to.be.closeTo(out![0], 0.1)
@@ -79,7 +69,7 @@ describe("world/screen coordinate system conversions", () => {
         mat4.rotateZ(camera, camera, deg2rad(30))
         mat4.translate(camera, camera, vec3.fromValues(1, 2, -5))
 
-        const out = doIt(point, perspective, camera, canvas)
+        const out = fromWorldToScreenAndBack(point, perspective, camera, canvas)
         // console.log(vec3.str(out))
 
         expect(point[0]).to.be.closeTo(out![0], 0.1)
@@ -88,7 +78,7 @@ describe("world/screen coordinate system conversions", () => {
     })
 })
 
-function doIt(point: vec3, perspective: mat4, camera: mat4, canvas: Size) {
+function fromWorldToScreenAndBack(point: vec3, perspective: mat4, camera: mat4, canvas: Size) {
     const perspectiveCamera = mat4.multiply(mat4.create(), perspective, camera)
     const screen = world2screen(point, perspectiveCamera, canvas)
     return screen2pointInPlane(screen, point, perspective, camera, canvas)
